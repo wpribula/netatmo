0746cb03f2fe44196d99261bd0edaee4c7eb46ec
Wojciech Pribula <wojciechpribula@gmail.com>
2023-03-27 21:04:11 +0200

    CHMI Temperature loading

--- src/netatmo/temperature/download_from_chmu.py
+++ src/netatmo/temperature/download_from_chmu.py
@@ -1,51 +1,152 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import requests
+import re
+from datetime import datetime, timedelta
+from bs4 import BeautifulSoup
 
-
-class Temperature:
-    def __init__(self, stanice : str):
-        # self.url = f" "
-        self.url = "F:\Downloads\O1MOSN01.xlsx"
-        self._read_temperature()
-        
-        
-    def _read_temperature(self):
-        self.data_df = pd.read_excel(self.url, 1)
-        self.data_df.drop([0,1,2], inplace=True)
-        self.data_df.columns = ['year', 'month'] + [i for i in range(1,32)]
-        self.data_df = self.data_df.melt(id_vars=['year','month'], var_name='day', value_name='temperature')
-        self.data_df.dropna(inplace=True)
-        self.data_df['year'] = self.data_df['year'].astype(int)
-        self.data_df['month'] = self.data_df['month'].astype(int)
-        self.data_df['day'] = self.data_df['day'].astype(int)
-        self.data_df['timestamp'] = self.data_df.apply(lambda row: pd.Timestamp(year=int(row['year']), month=int(row['month']), day=int(row['day'])), axis=1)
-        self.data_df.set_index('timestamp', drop=True, inplace=True)
-        self.data_df.sort_index(inplace=True)
+# class ChmiStations:
+#     def __init__(self):
+#         self._url = r"https://www.chmi.cz/aktualni-situace/aktualni-stav-pocasi/ceska-republika/souhrnny-prehled"
+#         self._base_url = r"https://www.chmi.cz"
+#         self.links = self._load_links()
     
-    def getTemperature(self) -> pd.Series:
-        return self.data_df['temperature']
+    
+#     def _load_site(self) -> list[BeautifulSoup]:
+#         html = requests.get(self._url)
+#         soup = BeautifulSoup(html.content, 'html.parser')
+#         return soup.find_all(name='ul', attrs={'class':'detached-menu'})
+        
+        
+#     def _load_links(self) -> list[BeautifulSoup]:
+#         menu = self._load_site()
+#         links = {}
+#         for menu_item in menu:
+#             links.update(self._extract_links(menu_item.find_all(name='li', attrs={'class':None})))
+#         return links
+        
+        
+#     def _extract_links(self, items : list[BeautifulSoup]) -> dict:
+#         links = {}
+#         for item in items:
+#             link = item.find(name='a')
+#             links.update({link.text:link.attrs['href']})
+#         return self._filter_links(links)
+    
+    
+#     def _filter_links(self, links : dict) -> dict:
+#         new_links = {}
+#         for key, link in links.items():
+#             if 'profesionalni-stanice/prehled-stanic' in link:
+#                 new_links.update({key:self._base_url + link})
+#         return new_links
+    
+    
+class ChmiStations:
+    def __init__(self):
+        self._url = r"https://www.chmi.cz/files/portal/docs/meteo/opss/pocasicko_nove/st_zemepis_cz.html"
+        self._base_url = r"https://www.chmi.cz"
+        self.stations = {}
+        self._load_stations()
+    
+    
+    def _load_stations(self) -> list[BeautifulSoup]:
+        rows = self._load_rows()
+        links = {}
+        for row in rows:
+            attributes = self._parse_row(row)
+            self.stations.update({attributes['name']:Station(attributes)})
+        return links
+    
+        
+    def _load_rows(self) -> list[BeautifulSoup]:
+        html = requests.get(self._url).content
+        soup = BeautifulSoup(html, 'html.parser')
+        return soup.find_all(name='tr', attrs={'class':'nezvyraznit'})
+        
+        
+    def _parse_row(self, row : BeautifulSoup) -> dict:
+        cels = row.find_all(name='td')
+        return {
+            'name':cels[0].text,
+            'base_url':self._base_url,
+            'url':cels[1].next.attrs['href'],
+            'latitude':float(cels[2].text[:-1].replace(',','.')),
+            'longitude':float(cels[4].text[:-1].replace(',','.')),
+            'height':float(cels[6].text.replace(',','.'))
+        }
+        
+    
+
+    
+            
+class Station:
+    def __init__(self, attributes : dict):
+        self.name = attributes['name']
+        self._base_url = attributes['base_url']
+        self.url = self._base_url + attributes['url']
+        self.latitude = attributes['latitude']
+        self.longitude = attributes['longitude']
+        self.height = attributes['height']
+        self.url = "https://www.chmi.cz/files/portal/docs/meteo/opss/pocasicko_nove/st_11406_cz.html"
+        self.station_number = self._get_station_number()
+        self.data_df = pd.DataFrame
+        self._load_data()
+        
+    def _get_station_number(self) -> str:
+        html = requests.get(self.url).content
+        graf_txt =  BeautifulSoup(html, 'html.parser').find_all(name='img')[-1].attrs['src']
+        return re.findall(r"\./st_(\d*)_grafy/graf_10_cz.png", graf_txt)[0]
+        
+    def _get_station_data_url(self) -> str:
+        return self._base_url + f"/files/portal/docs/meteo/opss/pocasicko_nove/st_{self.station_number}_cz.html"
+                    
+                    
+    def _load_data(self):
+        html = requests.get(self._get_station_data_url()).content
+        tables = BeautifulSoup(html, 'html.parser').find_all(name='table')
+        self._parse_time(tables[0])
+        self._parse_hourly_data(tables[1])
+        self._parse_daily_data(tables[2])
+        
+        
+        i = 1
+        
+    def _parse_time(self, table : BeautifulSoup) -> dict:
+        time_str = table.find_all(name='td')[2].text
+        time_str = re.findall("^(\d+. \d+. \d+ \d+:\d+) .*", time_str)[0]
+        time = datetime.strptime(time_str, "%d. %m. %Y %H:%M")
+        self.current_time = time
+
+
+    def _parse_hourly_data(self, table : BeautifulSoup) -> dict:
+        for row in table.find_all(name='tr')[1:]:
+            self._parse_hourly_data_row(row)
+    
+    
+    def _parse_hourly_data_row(self, row : BeautifulSoup) -> pd.DataFrame:
+        cells = row.find_all(name='td')
+        values = []
+        for i, cell in enumerate(cells[2:]):
+            values.append(self._parse_value_cell(cell.text, i))
+        data_df = pd.DataFrame(values)
+        data_df['value_name'] = cells[0].text
+        self.data_df = pd.concat([self.data_df, data_df])
+            
+            
+    def _parse_value_cell(self, cell : str, hour : int) -> dict:
+        values = cell.replace(',','.').split(' ')
+        return {
+            'timestamp' : self.current_time - timedelta(hours=hour),
+            'value' : float(values[0]),
+            'uom' : values[1]
+        }
+        
+            
+        
         
         
 if __name__ == "__main__":
-    stanice = "O1MOSN01"
-    t = Temperature(stanice)
-    # t.getTemperature().plot()
-    # plt.show()
-    
-    
-    fig = plt.figure(figsize=(8,8))
-    min_year = t.data_df['year'].min()
-    for year, data in t.data_df.groupby('year'):
-        data = data.groupby('month').agg({'temperature':'mean', 'day':'min'})
-        # theta = (data.apply(lambda row: (row.name - pd.Timestamp(year=int(row['year']), month=1, day=1)).days + 0, axis=1) / 365 * 2 * np.pi).values
-        theta = np.linspace(0,2*np.pi,12)
-        r = data['temperature'].values
-        ax1 = plt.subplot(111,projection="polar")
-        ax1.plot(theta, r, c=plt.cm.viridis((year - min_year) * 10))
-    ax1.set_xticklabels([str(month) for month in range(1,13)])
-    ax1.set_xticks(np.linspace(0,2*np.pi,13))
-    fig.set_facecolor("#323331")
-    ax1.set_facecolor("#000100")
-    plt.show()
+    ChmiStations()
     i = 1
--- src/netatmo/temperature/load_gas_consumption.py
+++ src/netatmo/temperature/load_gas_consumption.py
@@ -1,15 +1,57 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
 
 class GasConsumption:
-    def __init__(self)
+    def __init__(self):
         self._path = r"C:\Users\Wojciech\OneDrive\Dokumenty\Uczetnictwi\Energie\Spotrzeba plynu.xlsx"
+        self._temp_path = r"F:\Downloads\export.csv"
         self.data_df = pd.DataFrame()
-        self._laod_data()
+        self._load_data()
+        self.sampled_s = self._sample_data()
+        self.temperature_s = self._load_temperature()
+        
         
     def _load_data(self):
         self.data_df = pd.read_excel(self._path)
+        self.data_df.set_index('Time', drop=True, inplace=True)
+        
+        
+    def _sample_data(self):
+        start_date = self.data_df.index.date.min()
+        index = pd.date_range(start_date, pd.Timestamp.now(), freq='D')
+        data_s = pd.Series(np.NaN, index=index)
+        data_s = pd.concat([data_s, self.data_df['Value']])
+        data_s.sort_index(inplace=True)
+        # latest_valid_index = data_s.loc[~data_s.isna()].index[-1]
+        data_s.interpolate('time', limit_area='inside', inplace=True)
+        # data_s.drop(index=data_s.loc[data_s.index > latest_valid_index].index)
+        data_s.drop(index=data_s.loc[data_s.index.hour > 0].index, inplace=True)
+        return data_s.diff(1).shift(-1).dropna()
+    
+    
+    def _load_temperature(self) -> pd.Series:
+        temp_df = pd.read_csv(self._temp_path)
+        temp_df['date'] = pd.to_datetime(temp_df['date'])
+        temp_df.set_index('date', drop=True, inplace=True)
+        return temp_df['tavg']
+    
+    
+    def get_figure(self) -> Figure:
+        fig = plt.figure(figsize=(9,10))
+        axs = fig.subplots(1)
+        axs.plot(self.sampled_s, label='Gas consumption')
+        axs.twinx().plot(self.temperature_s, color='orange', label='Avg temperature')
+        axs.grid()
+        fig.legend()
+        return fig
+        
+    
+        
         
         
 if __name__ == "__main__":
     gc = GasConsumption()
-    print(gc.data_df)
+    gc.get_figure().show()
+    i = 1
